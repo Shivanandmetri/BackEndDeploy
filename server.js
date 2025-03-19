8b847324ac54f6efd0ee83bb4845070195937826
Shivanandmetri <shivanand1998metri@gmail.com>
2025-03-19 10:41:26 +0530

    Routes file added

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const userRoutes = require("./Routes/users");
 require("dotenv").config();
 const PORT = process.env.PORT;
+
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
